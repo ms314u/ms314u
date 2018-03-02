8fcdaaa241f33960ddec21ed901e594f68669ff2
Alexandr M <35875705+ms314u@users.noreply.github.com>
2018-03-02 09:55:57 +0200

    Update fft.R

--- 2018-02-03-fourier-predict/fft.R
+++ 2018-02-03-fourier-predict/fft.R
@@ -1,7 +1,5 @@
-#library(ggplot2)
-
-
-#http://www.ms314u.me/2018/03/02/monitoring-signals-deviations-from-the-normal-behavior/
+# Code for the post
+# http://www.ms314u.me/2018/03/02/monitoring-signals-deviations-from-the-normal-behavior/
 
 getCoeffs <- function(y){
   N <- length(y)
@@ -38,33 +36,3 @@ dat <- c(dat, rep(NA, 30))
 
 plot(predictions,type='l', cex=.35, xlab = "t", ylab = "Y", col="red")
 lines(dat, col="black")
-
-#t = seq(1, length(dat))
-#pdat = data.frame(x=t, y=dat, y2 = predictions)
-#p <- ggplot(data = pdat, aes(x=x, y=y)) + theme_bw()
-#p <- p + xlab("Time") + ylab("Value")
-#p <- p + geom_line()
-#p <- p + geom_line(aes(y=predictions), colour = "red")
-#p
-
-#lines(dat - predictions)
-
-# Reconstruct Signal - pmax is number of frequencies used in increasing order
-#pmax <- 30
-#fourierEstim <- rep(0,N)
-#for(n in 1:N){
-#  fourierEstim[n] <- A[1]/2
-#  for(p in 2:pmax){
-#    fourierEstim[n] <- fourierEstim[n] + A[p]*cos(2*pi*(p-1)*n/N) + B[p]*sin(2*pi*(p-1)*n/N)
-#  }
-#}
-#plot(dat,type='l', cex=.35, xlab = "t", ylab = "Y")
-#lines(fourierEstim, col="red")
-#lines(dat - fourierEstim)
-
-#for(n in 1:N){
-#  fourierEstim[n] <- A[1]/2
-#  for(p in 2:maxComponents){
-#    fourierEstim[n] <- fourierEstim[n] + A[p]*cos(2*pi*(p-1)*n/N) + B[p]*sin(2*pi*(p-1)*n/N)
-#  }
-#}
